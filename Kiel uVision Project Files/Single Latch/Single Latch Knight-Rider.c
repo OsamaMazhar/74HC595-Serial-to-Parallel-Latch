396ee55a2168248195aadc72e6d63cb3637a0ce4
osamazhar <osamazhar@yahoo.com>
2016-01-28 23:59:09 +0100

    uploaded

---
+++ Kiel uVision Project Files/Single Latch/Single Latch Knight-Rider.c
@@ -0,0 +1,59 @@
+#include<reg51.h>
+#include<math.h>
+
+sbit SH = P1^0;
+sbit ST = P1^1;
+sbit DS = P1^2;
+sbit LSBACC = ACC^0;
+
+void SerialIn(unsigned char);
+void LatchData(unsigned char LatchOutput);
+void MSDelay(unsigned int value); 
+
+void main()
+	{
+	char x;
+	SH = 0;
+	ST = 0;
+	DS = 0;
+	while(1)
+	{
+	for(x=0; x<8; x++)
+		{
+		 LatchData(pow(2,x));
+		 MSDelay(10);
+		}
+	for(x=6; x>=0; x--)
+		{
+		 LatchData(pow(2,x));
+		 MSDelay(10);
+		}
+	}
+   }
+ void SerialIn(unsigned char x)
+	{
+	 DS = x;
+	 SH = 1;
+	 DS = 0;
+ 	 SH = 0;
+	}
+
+void LatchData(unsigned char LatchOutput)
+	{
+	 unsigned char x;
+	 for(x=0; x<8; x++)
+	 	{
+	 	 ACC = LatchOutput;
+	 	 ACC = ACC >> x;
+	 	 SerialIn(LSBACC);
+		}
+	 ST = 1;
+	 ST = 0;
+	}
+
+void MSDelay(unsigned int value)
+	{
+		unsigned int x,y;
+		for(x=0; x<value; x++)
+			for(y=0; y<1275; y++);
+	}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# 74HC595-Serial-to-Parallel-Latch
-Single and Concatenated Serial to Parallel Latch is developed using 89C51 and 74HC595 IC. 
+# 74HC595 Based Serial-to-Parallel Latch
+
+This project utilizes 74HC595 to develop a serial-to-parallel latch. The IC requires on three hardware connections with the micrcontroller (may it be 8051 family or any other) and can extend the output hardware pins "theoretically" to infinite numbers. This project is very useful when developing our own displays with 7-segments or LED Matrix. Also it is useful for use with development boards like Arduino as they have limited I/O pins usually. Following is the link to the youtube video of this project which explains the concept and the code in sufficient detail:
+
+https://www.youtube.com/watch?v=RgIF0Xjdpjg
+
+
